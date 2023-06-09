46496da40ca94c9a92f3e0b534d8f7b8d31bdf43
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-06-09 16:32:30 -0300

    update, ready for deploy

--- src/components/characters.js
+++ src/components/characters.js
@@ -1,17 +1,12 @@
 import React, {useState, useEffect} from 'react'
 
 const Characters = () => {
-
-    //public key = 7fd6829ec8cfb0b586b532707b5fd01e
-    //private key = 5c912ba082eec0e9cdfefe45b544811bf35ce2be
-    //hash = bfa6d4a1d36b6069beff4df09974fb82
-    //ts = 1
     
     const [character, setCharacter] = useState([])
 
     useEffect(() => {
-        
-        fetch('https://gateway.marvel.com/v1/public/characters?limit=100&offset=300&ts=1&apikey=7fd6829ec8cfb0b586b532707b5fd01e&hash=bfa6d4a1d36b6069beff4df09974fb82')
+        fetch('https://gateway.marvel.com/v1/public/characters?limit=39&offset=1239&ts=1&apikey=7fd6829ec8cfb0b586b532707b5fd01e&hash=bfa6d4a1d36b6069beff4df09974fb82')
+        //fetch('https://gateway.marvel.com:443/v1/public/series/489/characters?ts=1&apikey=7fd6829ec8cfb0b586b532707b5fd01e&hash=bfa6d4a1d36b6069beff4df09974fb82') POR SERIE
             .then(response => response.json())
             .then(data => setCharacter(data.data.results))
 
